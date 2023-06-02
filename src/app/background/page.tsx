49ee6c2371412aa46a2dae3b1f4e795310a3b216
Kento Grüner <kento.s.gruener@gmail.com>
2023-06-02 14:47:57 +0200

    Pizza

--- src/app/background/page.tsx
+++ src/app/background/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import Clock from '@/components/Clock';
 import Background from '@/components/layout/Background';
 
 export default function Blank() {
   return (
     <>
-      <Clock />
       <Background />
     </>
   );
--- src/app/pizza/page.tsx
+++ src/app/pizza/page.tsx
@@ -1,23 +1,122 @@
 'use client';
 
+import { Title } from '@/components/text';
 import { useAppContext } from '@/context/AppContext';
+import styled from 'styled-components';
+
+function HeaderRow() {
+  return (
+    <Row>
+      <Cell>
+        <HeaderText>Name</HeaderText>
+      </Cell>
+      <Cell>
+        <HeaderText>Normal</HeaderText>
+      </Cell>
+      <Cell>
+        <HeaderText>Large</HeaderText>
+      </Cell>
+      <Cell>
+        <HeaderText>Family</HeaderText>
+      </Cell>
+      <Cell>
+        <HeaderText>Prima</HeaderText>
+      </Cell>
+    </Row>
+  );
+}
 
 export default function PizzaPage() {
   const { pizza } = useAppContext();
 
   return (
-    <>
-      {pizza.map((pizza) => {
-        return (
-          <div key={pizza.name}>
-            <h2>{pizza.name}</h2>
-            <p>Normal: {pizza.normal}</p>
-            <p>Large: {pizza.large}</p>
-            <p>Family: {pizza.family}</p>
-            <p>Prima: {pizza.prima}</p>
-          </div>
-        );
-      })}
-    </>
+    <ViewPort>
+      <Header>
+        <Title>Pizza</Title>
+      </Header>
+      <Content>
+        <HeaderRow />
+        {pizza.map((pizza) => (
+          <Row key={pizza.name}>
+            <Cell>
+              <TableText>{pizza.name}</TableText>
+            </Cell>
+            <Cell>
+              <TableText>{pizza.normal}</TableText>
+            </Cell>
+            <Cell>
+              <TableText>{pizza.large}</TableText>
+            </Cell>
+            <Cell>
+              <TableText>{pizza.family}</TableText>
+            </Cell>
+            <Cell>
+              <TableText>{pizza.prima}</TableText>
+            </Cell>
+          </Row>
+        ))}
+      </Content>
+      <Footer></Footer>
+    </ViewPort>
   );
 }
+
+const ViewPort = styled.div`
+  position: absolute;
+
+  height: 100vh;
+  width: 100vw;
+
+  display: grid;
+  grid-template-columns: 1fr 70% 1fr;
+  grid-template-rows: 1fr 70% 1fr;
+  grid-template-areas:
+    'header header header'
+    'left content right'
+    'footer footer footer';
+
+  background: rgba(0, 0, 0, 0.8);
+`;
+
+const Header = styled.div`
+  grid-area: header;
+  background: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  grid-area: content;
+  background: blue;
+`;
+
+const Footer = styled.div`
+  grid-area: footer;
+  background: green;
+`;
+
+const Row = styled.div`
+  display: grid;
+  height: 64px;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+const Cell = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 3rem;
+  font-weight: 700;
+  color: #fff;
+`;
+
+const TableText = styled.p`
+  font-size: 2rem;
+  font-weight: 700;
+  color: #fff;
+`;
